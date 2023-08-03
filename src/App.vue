eeca33449feb4dcafe7a4879a0b833e47efbc341
Ahmet Kaan Akgüloğlu <kaan.akguloglu@trk.technology>
2023-08-03 17:59:30 +0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <Bar :data="data" :options="options" />
+</template>
+
+<script lang="ts">
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+import { Bar } from 'vue-chartjs'
+import {data, options} from "./chartConfig.js";
+import zoomPlugin from 'chartjs-plugin-zoom';
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+ChartJS.register(zoomPlugin)
+
+export default {
+  name: 'App',
+  computed: {
+    options() {
+      return options
+    },
+    data() {
+      return data
+    }
+  },
+  components: {
+    Bar
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,26 @@
+import {h, createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import singleSpaVue from "single-spa-vue";
+
+createApp(App).mount('#app')
+
+const vueLifecycles = singleSpaVue({
+    createApp,
+    appOptions: {
+        render() {
+            return h(App, {
+                props: {
+                    // single-spa props are available on the "this" object. Forward them to your component as needed.
+                    // https://single-spa.js.org/docs/building-applications#lifecyle-props
+                    name: this.name,
+                    mountParcel: this.mountParcel,
+                    singleSpa: this.singleSpa,
+                },
+            });
+        },
+    },
+});
+export const bootstrap = vueLifecycles.bootstrap;
+export const mount = vueLifecycles.mount;
+export const unmount = vueLifecycles.unmount;
