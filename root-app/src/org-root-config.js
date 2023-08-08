2947dfc9b88e13b01d9205468d6aacaa42c77393
Ahmet Kaan Akgüloğlu <kaan.akguloglu@trk.technology>
2023-08-08 10:10:29 +0300

    add sub-app folder and root app in root-app folder

---
+++ root-app/src/org-root-config.js
@@ -0,0 +1,20 @@
+import { registerApplication, start } from "single-spa";
+
+// registerApplication({
+//   name: "@single-spa/welcome",
+//   app: () =>
+//     System.import(
+//       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+//     ),
+//   activeWhen: ["/"],
+// });
+
+registerApplication({
+  name: "zoom-app",
+  app: () => import(/* webpackIgnore: true */ "http://localhost:5173/src/main.js"),
+  activeWhen: ["/zoom-app"],
+});
+
+start({
+  urlRerouteOnly: true,
+});
